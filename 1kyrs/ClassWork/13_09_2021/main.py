3f8584e7bb39bb1260d0fa987b321281a25a0945
ChesterShelby <NazarovvorazaN@mail.ru>
2021-09-19 19:20:51 +0300

    Classwork and Homework 13-09-21 (Добавил поиск по клиентам)

--- 1kyrs/ClassWork/13_09_2021/main.py
+++ 1kyrs/ClassWork/13_09_2021/main.py
@@ -27,20 +27,27 @@ def add(db, name, number_phone, location, cost, driver_name, driver_number_phone
         'location' : f'{location}'
     })
     db.get('db').get('Apps').append({'client name': f'{client_name}', 'cost of trip': f'{cost}'})
-    db.get('db').get('Bank').append({'cost': f'{cost}'})
+    db.get('db').get('Bank').append({'client name': f'{client_name}', 'cost': f'{cost}'})
     db.get('db').get('Driver').append({
+        'client name': f'{client_name}',
         'driver name': f'{driver_name}',
         'number phone': f'{driver_number_phone}',
         'car': f'{car}',
         'model car': f'{model_car}'
     })
-    db.get('db').get('Company').append({'account': f'{account}'})
+    db.get('db').get('Company').append({'client name': f'{client_name}', 'account': f'{account}'})
 
     with open('db.json', 'w', encoding='utf-8') as f:
         json.dump(db, f, ensure_ascii=False)
 
 
-a = 0
+a = int(input('\n'
+              '--------------------\n'
+              ' 0 - Добавить пользователя  \n'
+              ' 1 - Закончить \n'
+              ' 2 - Поиск клиента\n'
+              '--------------------\n\n'))
+
 
 while a == 0:
     try:
@@ -51,8 +58,9 @@ while a == 0:
         add(db_new, name, number_phone, location, cost, driver_name, driver_number_phone, car, model_car, account)
         a = int(input('\n'
                       '--------------------\n'
-                      'Добавить еще пользователя 0 \n'
-                      ' 1 - закончить \n'
+                      ' 0 - Добавить еще пользователя  \n'
+                      ' 1 - Закончить \n'
+                      ' 2 - Поиск клиента\n'
                       '--------------------\n\n'))
 
     except FileNotFoundError:
@@ -74,6 +82,28 @@ while a == 0:
         add(db, name, number_phone, location, cost, driver_name, driver_number_phone, car, model_car, account)
         a = int(input('\n'
                       '--------------------\n'
-                      'Добавить еще пользователя 0 \n'
-                      ' 1 - закончить \n'
+                      ' 0 - Добавить еще пользователя  \n'
+                      ' 1 - Закончить \n'
+                      ' 2 - Поиск клиента\n'
                       '--------------------\n\n'))
+
+
+while a == 2:
+
+    with open('db.json', 'r', encoding='utf-8') as f:
+        db = json.load(f)
+    find_list = []
+    word = input('Имя клиента\t')
+    lst = ['clients', 'Apps', 'Bank', 'Driver', 'Company']
+    for j in lst:
+        clients_list = db.get('db').get(f'{j}')
+        for i in clients_list:
+            if i.get('client name') == word:
+                find_list.append(i)
+    print(find_list)
+    a = int(input('\n'
+                  '--------------------\n'
+                  ' 0 - Добавить еще пользователя  \n'
+                  ' 1 - Закончить \n'
+                  ' 2 - Поиск клиента\n'
+                  '--------------------\n\n'))
